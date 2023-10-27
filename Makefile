0123a6f0ea26a6665fef58a097cf0ff146cbf88e
m0o0n1 <kir8ox@gmail.com>
2023-10-27 22:16:39 +0300

    Fat is on the disk

--- Makefile
+++ Makefile
@@ -5,15 +5,25 @@ TARGET=os.bin
 ASM=nasm
 CC=i686-elf-gcc
 
-all: $(TARGET) clean
+all: $(BUILDDIR)/$(TARGET) clean
 
-$(TARGET): $(BUILDDIR)/bootloader.bin
-	dd if=/dev/zero of=$(BUILDDIR)/$(TARGET) bs=512 count=65536
-	mkfs.fat -s 2 -S 512 -F 16 $(BUILDDIR)/$(TARGET)
+$(BUILDDIR)/$(TARGET): $(BUILDDIR)/bootloader.bin
+	dd if=/dev/zero of=$(BUILDDIR)/$(TARGET) bs=512 count=5734
+	mkfs.fat -s 1 -S 512 -F 16 $(BUILDDIR)/$(TARGET)
 	dd if=$(BUILDDIR)/bootloader.bin of=$(BUILDDIR)/$(TARGET) conv=notrunc
 
 $(BUILDDIR)/bootloader.bin: $(PROJDIR)/boot/boot.asm
 	$(ASM) -f bin $(PROJDIR)/boot/boot.asm -o $(BUILDDIR)/bootloader.bin
+
+qemu:
+	qemu-system-i386 -fda $(BUILDDIR)/$(TARGET) -display curses -S -s
+
+gdb: 	
+		gdb -ex 'target remote localhost:1234' \
+		-ex 'set architecture i8086' \
+		-ex 'set disassembly-flavor intel' \
+		-ex 'break *0x7c00' \
+		-ex 'continue'	
 
 clean:
 	find $(BUILDDIR)/ ! -name $(TARGET) -type f -exec rm -f {} +
--- boot/boot.asm
+++ boot/boot.asm
@@ -7,17 +7,17 @@ nop
 bpb:
 .oem: db "IGNORED "
 .bytes_per_sector: dw 0x0200
-.sectors_per_cluster: db 0x02
-.number_of_reserved: dw 0x0002
+.sectors_per_cluster: db 0x01
+.number_of_reserved: dw 0x0001
 .number_of_fats: db 0x02
 .number_of_dir_entries: dw 0x0200
-.numer_of_sectors_volume: dw 0x0000
-.mdeia_decr_type: db 0xF8
-.number_of_sectors_per_fat: dw 0x0080
-.number_of_sectors_per_track: dw 0x0020
-.number_of_heads: dw 0x0004
-.number_of_hidden_sectors: dd 0x00000000
-.large_sector_count: dd 0x00010000
+.sectors_per_volume: dw 0x1660
+.media_descr_type: db 0xF8
+.sectors_per_fat: dw 0x0017
+.sectors_per_track: dw 0x0020
+.number_of_heads: dw 0x0002
+.number_of_hidden_sectors: dd 0x00
+.large_sector_count: dd 0x00
 
 ebr:
 .drive_number: db 0x00
@@ -29,6 +29,9 @@ ebr:
 
 
 
+root_dir_lba: dw 0x00
+data_lba: dw 0x00
+
 _start:
     mov [ebr.drive_number], dl
 
@@ -36,11 +39,111 @@ _start:
     mov al, 03h
     int 10h
 
-    
+    mov ah, 013h
+    mov al, 01h 
+    xor bx, bx
+    mov cx, 12  ; 12 -- size of the message to print
+    xor dx, dx
+    mov bp, boot_msg
+    int 010h
 
+    xor ax, ax
+    mov es, ax
+    mov ds, ax
+
+    mov ax, [bpb.number_of_fats]
+    mul word [bpb.sectors_per_fat]
+    add ax, [bpb.number_of_hidden_sectors]
+    add ax, [bpb.number_of_reserved]
+    mov [root_dir_lba], ax
+
+    mov ax, 0x20
+    mul word [bpb.number_of_dir_entries]
+    mov bx, [bpb.bytes_per_sector]
+    add ax, bx
+    ;dec ax
+    div bx
+    
+    add ax, word [root_dir_lba]
+    mov [data_lba], ax
+
+    mov ax, [bpb.sectors_per_fat]
+    mul word [bpb.number_of_fats]
+    mov cx, [bpb.number_of_reserved]
+    xchg cx, ax
+    mov bx, 0x500
+    mov dl, byte [ebr.drive_number]
+    call disk_read
 
     jmp $
 
+; in
+;   ax - lba
+; out
+;   CH = low eight bits of cylinder number
+;   CL = sector number 1-63 (bits 0-5)
+;   DH = head number
+lba_to_chs:
+    push ax
+    push dx
 
+    xor dx, dx
+    div word [bpb.sectors_per_track] ; ax = lba / spt
+    inc dx                      ; dx = lba % spt + 1    = sector
+
+    mov cx, dx
+
+    xor dx, dx
+    div word [bpb.number_of_heads]   ; ax = cylinder, dx = head
+
+    mov dh, dl
+    mov ch, al
+    shl ah, 6
+    or cl, ah
+
+    pop ax
+    mov dl, al
+    pop ax
+    ret
+
+
+; in
+;   ax - lba
+;   cx - number of sectors to read
+;   dl - drive number
+;   es:bx - data buffer
+; out
+;   carry flag is set on error
+disk_read:
+    pusha
+    push cx
+    call lba_to_chs
+    stc
+    pop ax
+    mov ah, 02h
+    int 13h
+    jc .carry_happened
+    
+    popa
+    ret
+.carry_happened:
+    popa    
+error_happened:
+    mov ah, 013h
+    mov al, 01h 
+    xor bx, bx
+    mov cx, 03fh
+    xor dx, dx
+    mov bp, error_msg
+    int 010h
+
+    mov ah, 00h
+    int 016h
+
+    jmp 0ffffh:00h
+
+boot_msg: db "Booting...", 0x0a, 0x0d
+error_msg:  db "There was an error. Check your drive and press a key to reboot."
+sys_files: db "IO      SYSKERNEL  SYS"
 times 510 - ($ - $$) db 0x00
 dw 0xaa55
