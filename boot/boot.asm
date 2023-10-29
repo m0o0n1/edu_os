a7dd835c919493fe73a2f14a8bb6d315f6a4f9ad
m0o0n1 <kir8ox@gmail.com>
2023-10-29 15:26:53 +0300

    root dir

--- boot/boot.asm
+++ boot/boot.asm
@@ -14,8 +14,8 @@ bpb:
 .sectors_per_volume: dw 0x1660
 .media_descr_type: db 0xF8
 .sectors_per_fat: dw 0x0017
-.sectors_per_track: dw 0x0020
-.number_of_heads: dw 0x0002
+.sectors_per_track: dw 0x2000
+.number_of_heads: dw 0x0200
 .number_of_hidden_sectors: dd 0x00
 .large_sector_count: dd 0x00
 
@@ -35,6 +35,17 @@ data_lba: dw 0x00
 _start:
     mov [ebr.drive_number], dl
 
+    mov ah, 08h
+    int 13h
+    jc error_happened
+
+    and cl, 03fh
+    xor ch, ch
+    mov [bpb.sectors_per_track], cx;
+
+    inc dh
+    mov [bpb.number_of_heads], dh
+
     mov ah, 00h
     mov al, 03h
     int 10h
@@ -42,7 +53,7 @@ _start:
     mov ah, 013h
     mov al, 01h 
     xor bx, bx
-    mov cx, 12  ; 12 -- size of the message to print
+    mov cx, 12
     xor dx, dx
     mov bp, boot_msg
     int 010h
@@ -51,28 +62,41 @@ _start:
     mov es, ax
     mov ds, ax
 
-    mov ax, [bpb.number_of_fats]
-    mul word [bpb.sectors_per_fat]
-    add ax, [bpb.number_of_hidden_sectors]
-    add ax, [bpb.number_of_reserved]
-    mov [root_dir_lba], ax
-
-    mov ax, 0x20
-    mul word [bpb.number_of_dir_entries]
-    mov bx, [bpb.bytes_per_sector]
-    add ax, bx
-    ;dec ax
-    div bx
-    
-    add ax, word [root_dir_lba]
-    mov [data_lba], ax
-
+    ; load FAT in 0x00:0x500
     mov ax, [bpb.sectors_per_fat]
     mul word [bpb.number_of_fats]
+    jc error_happened
+
     mov cx, [bpb.number_of_reserved]
     xchg cx, ax
-    mov bx, 0x500
+    mov bx, [fat_address]
     mov dl, byte [ebr.drive_number]
+    call disk_read
+
+    ; calculate root_dir entry sector (lba)
+    mov ax, [bpb.number_of_fats]
+    mov bx, [bpb.sectors_per_fat]
+    mul bx
+    jc error_happened
+
+    add ax, [bpb.number_of_reserved]
+    push ax
+
+    ; how many sectors occupied by root_dir
+    mov ax, [bpb.number_of_dir_entries]
+    shl ax, 5
+    xor dx, dx
+    div word [bpb.bytes_per_sector]
+    test dx, dx
+    jz .root_dir_load
+
+    inc ax
+.root_dir_load:
+    mov cl, al  ; cl -- sectors to read
+
+    pop ax      ; ax -- lba of root_dir
+    mov bx, [rd_address]
+    mov dl, [ebr.drive_number]
     call disk_read
 
     jmp $
@@ -83,22 +107,22 @@ _start:
 ;   CH = low eight bits of cylinder number
 ;   CL = sector number 1-63 (bits 0-5)
 ;   DH = head number
-lba_to_chs:
+lba_to_chs: 
     push ax
     push dx
 
     xor dx, dx
-    div word [bpb.sectors_per_track] ; ax = lba / spt
-    inc dx                      ; dx = lba % spt + 1    = sector
+    div word [bpb.sectors_per_track]
+    inc dx                     
 
-    mov cx, dx
+    mov cx, dx     
 
     xor dx, dx
-    div word [bpb.number_of_heads]   ; ax = cylinder, dx = head
-
-    mov dh, dl
-    mov ch, al
-    shl ah, 6
+    div word [bpb.number_of_heads]   
+                                     
+    mov dh, dl                          
+    mov ch, al                          
+    shl ah, 6                           
     or cl, ah
 
     pop ax
@@ -114,7 +138,7 @@ lba_to_chs:
 ;   es:bx - data buffer
 ; out
 ;   carry flag is set on error
-disk_read:
+disk_read:  
     pusha
     push cx
     call lba_to_chs
@@ -142,6 +166,8 @@ error_happened:
 
     jmp 0ffffh:00h
 
+fat_address: dw 0x500
+rd_address: dw 0x7e00
 boot_msg: db "Booting...", 0x0a, 0x0d
 error_msg:  db "There was an error. Check your drive and press a key to reboot."
 sys_files: db "IO      SYSKERNEL  SYS"
